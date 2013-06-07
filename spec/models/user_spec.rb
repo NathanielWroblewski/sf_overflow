be828ab6a74f1944adb652aa71da78cb5a5108d6
ysadka <ysadka@gmail.com>
2013-06-07 15:03:40 -0700

    Shoulda User model spec - green

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,15 @@
 require 'spec_helper'
 
-describe "User" do
-  
-  describe "email" do
-    pending
-  end
+describe User do
 
-  describe "username" do
-    pending
-  end
+  it { should have_many(:questions) }
+  it { should have_many(:answers) }
+  it { should have_many(:votes) }
 
+  it { should validate_presence_of(:email) }
+  it { should validate_format_of(:email).with( /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/).with_message(/email is invalid/) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_presence_of(:username) }
+  it { should validate_uniqueness_of(:username) }
 
 end
