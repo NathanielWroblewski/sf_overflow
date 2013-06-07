d76c9960e820225d4c7dc0c747f712c839aaa891
adamsartell <adam.sartell@gmail.com>
2013-06-06 23:58:23 -0700

    refactored polymorphic votes and html templates

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,36 +2,15 @@ class VotesController < ApplicationController
   include ApplicationHelper
   include VotesHelper
 
-
   def upvote
-    voter(params, 1)
+    votable_type = votable_type(params)
+    tally_votes(votable_type, 1)
+    redirect_to votable_type
   end
 
   def downvote
-    voter(params, -1)
-  end
-
-  private
-
-  def voted(votable)
-    votable.votes.find_by_user_id(current_user.id)
-  end
-
-
-  def voter(params, score)
-    votable, q_id = votable_id_find(params)[0], votable_id_find(params)[1]
-    vote = voted(votable)
-    if voted(votable)
-      user_vote = votable.votes.find_by_user_id(current_user.id)
-      if vote.counter == 0
-        Vote.update(user_vote.id, :counter => score)
-      else
-        Vote.update(user_vote.id, :counter => 0)
-      end
-    else
-      votable.votes << Vote.create(user_id: current_user.id, counter: score)
-    end
-    question = Question.find(q_id)
-    redirect_to question_path(question)
+    votable_type = votable_type(params)
+    tally_votes(votable_type, -1)
+    redirect_to votable_type
   end
 end
--- db/migrate/20130606173854_create_votes.rb
+++ db/migrate/20130606173854_create_votes.rb
@@ -1,10 +1,9 @@
 class CreateVotes < ActiveRecord::Migration
   def change
-    create_table :votes do |t|
-      t.string :votable_type
-      t.integer :votable_id, :user_id, :counter
-
-      t.timestamps
+    create_table :votes do |column|
+      column.string :votable_type
+      column.integer :votable_id, :user_id, :counter
+      column.timestamps
     end
   end
 end
