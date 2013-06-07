aadd0fd5c143ed9e08546f6ed46f93250ce9ddcb
ysadka <ysadka@gmail.com>
2013-06-07 11:52:37 -0700

    Shoulda tests - passing

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
-describe "Question" do
-  #Build spec for question model
+describe Question do
+
+  it { should belong_to(:user) }
+
+  it { should have_many(:answers) }
+  it { should have_many(:votes) } 
+
+  it { should validate_presence_of(:title) }
+  it { should validate_uniqueness_of(:title) }
+  it { should validate_presence_of(:description) }
+
 end
