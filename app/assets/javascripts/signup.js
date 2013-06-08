f86d05adfc86d317636ad3fc60ed5d1a37b6d268
nathanielwroblewski <nathanielwroblewski@gmail.com>
2013-06-08 16:19:41 -0700

    removed js files

--- app/assets/javascripts/signup.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $("#signup_button").on("click", 'a', function(e){
-    $("#login_button").unbind();
-    e.preventDefault();
-    $(".signup_form").slideToggle();
-  });
-});
-
