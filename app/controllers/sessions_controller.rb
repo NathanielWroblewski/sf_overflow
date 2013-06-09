3e4b39487606c1d3933bfebe00ad58670e32178c
ysadka <ysadka@gmail.com>
2013-06-08 22:55:42 -0700

    Corrected Session to find by username, not email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
   def create
-    @user = User.find_by_email(params["email"])
+    @user = User.find_by_username(params["username"])
     if @user
       if @user.password = params["password"]
        session[:id] = @user.id
